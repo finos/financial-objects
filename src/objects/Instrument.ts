5b6a050ece4729ac757cc9ec7de2d4f4e15e479e
Johan Sandersson <jsandersson@factset.com>
2019-02-08 12:20:28 +0000

    Update to reflect the ratified v1 structure

--- src/objects/Instrument.ts
+++ src/objects/Instrument.ts
@@ -1,13 +1,10 @@
-import { Product } from "./Product";
-
 /**
  * Several use cases and workflows rely on that all parties involved can understand what instrument is being referred to - be it a simple chat message "why is Apple up today?" where the receiver wants to look up details about the company mentioned or a complex financial object - involving several securities - that will be parsed by a system.
  * An object with standardized names would allow for the use of several IDs to define the instrument - including all well-known data-sets and open symbology reference data as well as custom inhouse IDs if needed.
- * The main idea is for each participant to send as many IDs as exist in the system and be able to receive/understand as many as possible.This will ensure compatibility with existing applications while at the same time allow for a potential "most-favored" ID to become a future standard.
+ * The main idea is for each participant to send as many IDs as exist in the system and be able to receive/understand as many as possible. This will ensure compatibility with existing applications while at the same time allow for a potential "most-favored" ID to become a future standard.
  */
 export interface Instrument {
-  product: Product;
-  name: string;
+  name?: string;
   id: {
     ticker?: string;
     ISIN?: string;
@@ -17,5 +14,7 @@ export interface Instrument {
     BBG?: string;
     PERMID?: string;
     FIGI?: string;
+    com.factset.symbology.entity?: string;
+    com.factset.symbology.regionalticker?: string;
   };
 }
