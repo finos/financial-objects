e978cca66bd5959a1918872f1e875cca18b2a591
Johan Sandersson <jsandersson@factset.com>
2019-02-11 00:37:01 +0000

    Improved description

--- src/objects/Portfolio.ts
+++ src/objects/Portfolio.ts
@@ -1,7 +1,7 @@
 import { Position } from "./Position";
 
 /**
- * A standardized format to transfer Positions and unique meta data for each portfolio, with possibility to extend what is included for each portfolio, including custom inhouse IDs if needed.
+ * A standardized format to transfer Positions and unique meta data for a portfolio, with possibility to extend what is included for a portfolio, including custom inhouse IDs if needed.
  */
 export interface Portfolio {
   id: {
