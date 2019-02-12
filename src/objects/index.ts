cf31884b90f2a79ca77cfe5fd1ab61cd29f2cc8c
Johan Sandersson <jsandersson@factset.com>
2019-02-12 11:11:56 +0000

    Removing non-object

--- src/objects/index.ts
+++ src/objects/index.ts
@@ -1,9 +1,7 @@
 export { Contact } from "./Contact";
 export { ContactList } from "./ContactList";
 export { IRS } from "./IRS";
-export { Equity } from "./Equity";
 export { Country } from "./Country";
-export { Entity } from "./Entity";
 export { Instrument } from "./Instrument";
 export { InstrumentList } from "./InstrumentList";
 export { Order } from "./Order";
@@ -11,7 +9,6 @@ export { OrderResponse } from "./OrderResponse";
 export { Organization } from "./Organization";
 export { Portfolio } from "./Portfolio";
 export { Position } from "./Position";
-export { Product } from "./Product";
 export { RFQ } from "./RFQ";
 export { RFQResponse } from "./RFQResponse";
 export { Trade } from "./Trade";
